baae905fa2ecaf12c2332236792ba17cd64f5b00
Vd7905 <vikasdixit7905@gmail.com>
2025-11-03 20:42:40 +0530

    edited main.py

--- ML_Service/main.py
+++ ML_Service/main.py
@@ -42,7 +42,7 @@ def batch_recommend_endpoint(customers: List[CustomerData] = Body(...)):
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    PORT = int(os.getenv("PORT", 8002))
-    uvicorn.run(app, host="0.0.0.0", port=PORT, reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#     PORT = int(os.getenv("PORT", 8002))
+#     uvicorn.run(app, host="0.0.0.0", port=PORT, reload=True)
